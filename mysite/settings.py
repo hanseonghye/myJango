5d850bbd612abc533dfc6946e9841a2f1e6c3562
hanseonghye <hans12312@naver.com>
2019-06-17 07:14:08 +0900

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -53,10 +53,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mysite.urls'
 
+TEMPLATE_DIRS =[os.path.join(BASE_DIR,'templates')]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': TEMPLATE_DIRS,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
